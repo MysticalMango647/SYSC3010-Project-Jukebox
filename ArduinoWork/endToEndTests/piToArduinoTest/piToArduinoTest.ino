ba1796d4b78dd7745f38fca3f533585ecf1fc045
greatw1te2 <corbingarlough@cmail.carleton.ca>
2022-04-09 12:31:04 -0400

    Corbin docs: updating commenting

--- ArduinoWork/endToEndTests/piToArduinoTest/piToArduinoTest.ino
+++ ArduinoWork/endToEndTests/piToArduinoTest/piToArduinoTest.ino
@@ -1,10 +1,11 @@
+//This script runs on the arduino connected the the LCD screen, and tests communication between the Jukebox Pi and the Arduino
+//Written by: Corbin Garlough
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read messages from the Pi and reply with what was recieved
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
     Serial.print("The Arduino recieved: ");
